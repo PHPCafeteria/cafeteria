d383fb7d4f3bd251b461ab4a01e9642c255c12d9
AsmaaHamada3397 <asmaa.amin3397@gmail.com>
2024-08-18 15:05:55 +0300

    second edition

---
+++ asmaa/header/headerForUser.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>order user</title>
+    <link rel="stylesheet" href="all.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="cafeteria-logo.png" alt="cafeteria" width="50" height="40">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item   ">
+                        <a class="nav-link active text-white fs-6 " aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white fs-6" href="#">My Orders</a>
+                    </li>
+                </ul>
+            </div>
+            <!--add pic php-->
+            <div class="text-align-end d-flex">
+                <div class="px-3">
+                    <img class="rounded-circle border-2 border-black" src="cofee.png"
+                        alt="Profile Picture" style="width: 45px; height: 45px;">
+                </div>
+                <div>
+                    <a class="nav-link text-white fs-6 pt-3" href="#">username</a>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+    <script src="bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
