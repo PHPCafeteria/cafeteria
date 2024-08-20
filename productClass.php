12361fbe5059d9c678b4aaa452c3ec4ad069f075
davidaymanba <aymandavid850@gmail.com>
2024-08-21 01:46:00 +0200

    add button delete from user & product

--- productClass.php
+++ productClass.php
@@ -5,7 +5,7 @@
 
     require('../DB.php');
 
-    class Prudect{
+    class Product{
         private $name, $price, $available, $picture, $idCatogry;
         // id is auto-increament
         function __construct($name="", $price=0, $available=1, $picture="", $idCatogry=0){
@@ -40,19 +40,24 @@
 
         public function delete($id){
             $n = new ConnectionDB;
-            echo $n->Connection();
-            $sql = 'delete from product where idProduct = ?';
-            $stataus = $n->db->prepare($sql);
-            $stataus->execute([$id]);
-            // $result = $stataus->fetchAll(PDO::FETCH_ASSOC);
-            // echo '<br>';
-            print_r($result);
+            $n->Connection();
+            $sql = 'DELETE FROM product WHERE idProduct =?';
+            $stmt = $n->db->prepare($sql);
+            $result = $stmt->execute([$id]);
+            
+            if ($result) {
+                echo json_encode(['status' => 'success']);
+            } else {
+                http_response_code(500);
+                echo json_encode(['error' => 'Failed to delete product']);
+            }
         }
+        
 
         public function fetchProducts() {
             $t = new ConnectionDB;
             $t->Connection();
-            $sql = 'SELECT name, price, picture FROM product';
+            $sql = 'SELECT idProduct, name , price, picture FROM product';
             $stmt = $t->db->prepare($sql);
             $stmt->execute();
             return json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
@@ -61,4 +66,6 @@
 
     }
 ?>
+
+
 
